c284896cd7563054d567bfc7a5c45a253a0ef866
Jake <jakesarkar@hotmail.co.uk>
2022-02-26 19:31:02 +0000

    Sorting out tables

--- api/modules/accounts.js
+++ api/modules/accounts.js
@@ -14,19 +14,28 @@ export async function login(credentials) {
 	if(!records[0].count) throw new Error(`username "${user}" not found`)
 	sql = `SELECT pass FROM accounts WHERE user = "${user}";`
 	records = await db.query(sql)
+	sql = `SELECT pass FROM accounts WHERE user = "${user}";`
 	const valid = await compare(pass, records[0].pass)
+	//sql = `SELECT userType FROM accounts WHERE user = "${user}";`
+	//const userType = await db.query(sql)
+	//if (userType === "student") getStudent(credentials)
+	//else if (userType == "teacher") getTeacher(credentials)
 	if(valid === false) throw new Error(`invalid password for account "${user}"`)
 	return user
 }
 
 export async function register(credentials) {
 	credentials.pass = await hash(credentials.pass, salt)
-	const sql = `INSERT INTO accounts(user, pass) VALUES("${credentials.user}", "${credentials.pass}")`
-	console.log(sql)
-	await db.query(sql)
+	try {
+		const sql = `INSERT INTO accounts(user, pass, userType, isAdmin) VALUES("${credentials.user}", "${credentials.pass}", "${credentials.userType}", "${credentials.isAdmin}")`
+		console.log(sql)
+		await db.query(sql)
+	} catch(err) {
+		console.log(err)
+	}
 	return true
 }
 
-export async function getTeacher(credentials) {}
+//export async function getTeacher(credentials) {}
 
-export async function getStudent(credentials) {}
+//export async function getStudent(credentials) {}
